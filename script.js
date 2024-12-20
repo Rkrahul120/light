fe517d638c3c0a6f2724d8476952a40a86bfeea0
Rkrahul120 <satish2020@gift.edu.in>
2024-12-20 11:24:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function On(){
+    let img=document.getElementById("image")
+    img.src="on.png"
+    
+}
+function Off(){
+    let img=document.getElementById("image")
+    img.src="off.png"
+}  
